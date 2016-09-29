d33d1aa5fd2a99dcb85ce433a1cc98b93051636e
gness1804 <graham.nessler@gmail.com>
2016-09-29 16:27:27 -0600

    creates var to capture text in textarea

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
   const generateButton = $("#generate-button");
+  const copyTextButton = $("#copy-text-button");
 
   const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "suhdude ", "gradients! ", "blakement ", "game-time ", "ku sucks ", "blake street vault ", "carne asada ", "javascript tears ", "go blue ", "chaz-isms ", "magenta ", "rabbit holes ", "broncos ", "bicycles ", "daledalf ", "rainbow css vomit ", "star bar "];
 
@@ -47,6 +48,9 @@ $(document).ready(function () {
 
   }); // end of main generate function
 
-  
+  copyTextButton.on("click", function () {
+    let TextToCopy = $("textarea").val();
+    console.log(TextToCopy);
+  });
 
 }); //end of jQuery body
