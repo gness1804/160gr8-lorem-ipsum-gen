a4598677b0237b66e6b435197c7956f74e0241b1
gness1804 <graham.nessler@gmail.com>
2016-09-29 13:32:14 -0600

    adds random number generator function to lorem generation

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
-  const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "gradients! ", "blakement ", "game time ", "ku sucks ", "blake street vault ", "javascript tears ", "ku sucks again ", "chaz-isms"];
+  const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "gradients! ", "blakement ", "game time ", "ku sucks ", "blake street vault ", "javascript tears ", "ku sucks again ", "chaz-isms "];
 
   $("#generate-button").on("click", function () {
+    let random = function () {
+      let min = 1;
+      let max = 4;
+      return Math.floor(Math.random() * (max - min) + min);
+    };
+    console.log(random());
     for (var i = 0; i < text.length; i++) {
       $("textarea").append(text[i]);
     }
