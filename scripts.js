d21886498cbe398a634eb8c05dbffc8822374a82
gness1804 <graham.nessler@gmail.com>
2016-09-29 13:02:58 -0600

    generate function can log all words in array

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
   const text = ["garbage", "gabitron", "git sh*t", "bluecifer", "lingo", "retro", "gusto", "milkman", "kansas raptor"];
 
-
+  $("#generate-button").on("click", function () {
+    for (var i = 0; i < text.length; i++) {
+      
+    }
+  });
 
 
 
