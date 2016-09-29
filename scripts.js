144cb949bad8dffd4634955e6251f7dfedbf5440
gness1804 <graham.nessler@gmail.com>
2016-09-29 17:34:46 -0600

    background is now much better; linear gradient that covers whole page nicely

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   const generateButton = $("#generate-button");
   const copyTextButton = $("#copy-text-button");
 
-  const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "suhdude ", "gradients! ", "yoga instructor ", "blakement ", "game-time ", "ku sucks ", "blake street vault ", "carne asada ", "javascript tears ", "go blue ", "chaz-isms ", "magenta ", "rabbit holes ", "broncos ", "bicycles ", "daledalf ", "rainbow css vomit ", "star bar ", "k's horse "];
+  const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "suhdude ", "gradients! ", "yoga instructor ", "blakement ", "game-time ", "ku sucks ", "blake street vault ", "carne asada ", "javascript tears ", "go blue ", "chaz-isms ", "magenta ", "rabbit holes ", "broncos ", "bicycles ", "daledalf ", "rainbow css vomit ", "star bar ", "k's horse ", "bree's rules "];
 
   generateButton.on("click", function () {
 
