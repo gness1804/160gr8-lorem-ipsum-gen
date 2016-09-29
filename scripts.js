5e06aea0c8c86216506f366b937cb22d7bac3a85
gness1804 <graham.nessler@gmail.com>
2016-09-29 13:51:24 -0600

    first rng appears to be working

--- scripts.js
+++ scripts.js
@@ -7,18 +7,27 @@ $(document).ready(function () {
       return Math.random();
     }
 
-    
-
-    const random2 = function () {
-      let min = 0;
-      let max = 4;
-      return Math.floor(Math.random() * (max - min) + min);
-    };
-
-    for (var i = 0; i < text.length; i = i + random2()) {
-      $("textarea").append(text[i]);
+    if (random1() < 0.5) {
+      generateUp();
     }
-  });
+
+    function generateUp() {
+
+      const random2 = function () {
+        let min = 0;
+        let max = 4;
+        return Math.floor(Math.random() * (max - min) + min);
+      }; // end of random2
+
+      for (var i = 0; i < text.length; i = i + random2()) {
+        $("textarea").append(text[i]);
+      } // end of for loop
+
+    } // end of generateUp
+
+
+
+  }); // end of main generate function
 
 
 
