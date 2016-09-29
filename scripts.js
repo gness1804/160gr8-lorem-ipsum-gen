300ab823d1e85b2d263be609abbf8b587537e92a
gness1804 <graham.nessler@gmail.com>
2016-09-29 13:42:01 -0600

    some syntatical changes

--- scripts.js
+++ scripts.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
   const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "gradients! ", "blakement ", "game time ", "ku sucks ", "blake street vault ", "javascript tears ", "ku sucks again ", "chaz-isms "];
 
   $("#generate-button").on("click", function () {
-    let random = function () {
-      let min = 1;
+
+    const random = function () {
+      let min = 0;
       let max = 4;
       return Math.floor(Math.random() * (max - min) + min);
     };
-    console.log(random());
-    for (var i = 0; i < text.length; i++) {
+
+    for (var i = 0; i < text.length; i = i + random()) {
       $("textarea").append(text[i]);
     }
   });
