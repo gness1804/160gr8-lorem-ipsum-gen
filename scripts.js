1e9223e714e15143b84eeb38b0c5d4b1eb60dcf5
gness1804 <graham.nessler@gmail.com>
2016-09-29 19:46:59 -0600

    clear text button works

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
   const generateButton = $("#generate-button");
   const copyTextButton = $("#copy-text-button");
   const randomChoiceRadio = $("#random-choice");
+  const clearTextButton = $("#clear-text-from-textarea");
 
   const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "suhdude ", "gradients! ", "yoga instructor ", "blakement ", "game-time ", "ku sucks ", "blake street vault ", "carne asada ", "javascript tears ", "go blue ", "chaz-isms ", "magenta ", "rabbit holes ", "broncos ", "bicycles ", "champus ", "daledalf ", "rainbow css vomit ", "star bar ", "k's horse ", "bree's rules ", "ps lounge ", "merge conflicts ", "weird gifs ", "command line ", "stack overflow forever ", "monstertorium ", "mod 1 beards ", "dale's pale ale ", "bad wine ", "dressage "];
 
@@ -66,6 +67,10 @@ $(document).ready(function () {
     $("textarea").select();
     document.execCommand("copy");
     alert('Text copied!');
-  });
+  }); // end of copy text
+
+  clearTextButton.on("click", function () {
+    $("textarea").text("");
+  }); // end of clear text
 
 }); //end of jQuery body
