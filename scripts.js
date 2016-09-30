03db39901a0cc52cd121f904972bc84682e63b42
gness1804 <graham.nessler@gmail.com>
2016-09-29 18:01:07 -0600

    program now recognizes if random choice radio button is checked

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
 
   const generateButton = $("#generate-button");
   const copyTextButton = $("#copy-text-button");
-  const 
+  const randomChoiceRadio = $("#random-choice");
 
   const text = ["garbage ", "gabitron ", "git sh*t ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas raptor ", "NaN ", "suhdude ", "gradients! ", "yoga instructor ", "blakement ", "game-time ", "ku sucks ", "blake street vault ", "carne asada ", "javascript tears ", "go blue ", "chaz-isms ", "magenta ", "rabbit holes ", "broncos ", "bicycles ", "daledalf ", "rainbow css vomit ", "star bar ", "k's horse ", "bree's rules ", "ps lounge ", "merge conflicts ", "weird gifs ", "command line ", "stack overflow forever ", "monstertorium ", "mod 1 beards ", "dale's pale ale ", "bad wine ", "dressage "];
 
   generateButton.on("click", function () {
 
-    if (true) {
-
+    if (document.getElementById("random-choice").checked) {
+      alert('hi');
     }
 
     function random1() {
