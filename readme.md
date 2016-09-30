3799d617df56fa145402c3aff60786945fb0a509
gness1804 <graham.nessler@gmail.com>
2016-09-29 23:40:26 -0600

    adds new info to readme to reflect updated features

--- readme.md
+++ readme.md
@@ -5,7 +5,9 @@
 This is a lorem ipsum generator using various phrases heard around Gusto with the Turing 1608 (aka "160gr8") cohort. It uses 1608-isms that can then be copied and pasted as dummy text into your app/website (or just generated for fun).
 
 Features:
-* Clicking on "Advanced" button opens up options to either generate text in random order or to generate a certain number of words (1-100).
+* Clicking on "Advanced" button opens up options to either generate text in random order or to generate a user-specified number of words (1-100).
 * User can hide advanced options with "Hide These Options" button.
 * One button copies text in the text box to the user's clipboard.
 * Another button clears the text in the text box.
+* User can enter in a new word to the lorem ipsum generator (this is erased on page reload).
+* User can also see the current word count at the bottom of the text book (if no words are in the box, the word count does not display).
--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
   const copyTextButton = $("#copy-text-button");
   const clearTextButton = $("#clear-text-from-textarea");
 
-  const text = ["garbage ", "gabitron ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas-raptor ", "NaN ", "suhdude ", "gradients! ", "yoga-instructor ", "blakement ", "game-time ", "K.U.-sucks ", "blake-street-vault ", "carne-asada ", "javascript-tears ", "go-blue ", "chaz-isms ", "magenta ", "rabbit-holes ", "epically-bad-gusto-coffee ", "broncos ", "bicycles ", "champus ", "daledalf ", "rainbow-css-vomit ", "star-bar ", "k's-horse ", "bree's-tattoos ", "ps-lounge ", "merge-conflicts ", "weird-gifs ", "command-line ", "stack-overflow-forever ", "monstertorium ", "mod-1-beards ", "dale's-pale-ale ", "bad-wine ", "dressage ", "slack-attack ", "steve's-bad-day ", "where's-my-cubby ", "to-posse-or-not-to-posse ", "git-sh*t "];
+  const text = ["garbage ", "gabitron ", "bluecifer ", "lingo ", "retro ", "gusto ", "milkman ", "kansas-raptor ", "NaN ", "suhdude ", "gradients! ", "yoga-instructor ", "blakement ", "game-time ", "K.U.-sucks ", "blake-street-vault ", "carne-asada ", "javascript-tears ", "go-blue ", "chaz-isms ", "magenta ", "rabbit-holes ", "epically-bad-gusto-coffee ", "broncos ", "bicycles ", "champus ", "daledalf ", "rainbow-css-vomit ", "star-bar ", "k's-horse ", "bree's-tattoos ", "ps-lounge ", "merge-conflicts ", "weird-gifs ", "command-line ", "stack-overflow-forever ", "monstertorium ", "mod-1-beards ", "dale's-pale-ale ", "bad-wine ", "dressage ", "slack-attack ", "steve's-bad-day ", "where's-my-cubby ", "to-posse-or-not-to-posse ", "git-sh*t ", "modulo "];
 
   let wordsInBox = [];
 
@@ -63,11 +63,9 @@ $(document).ready(function () {
 
       if (random1() < 0.5) {
         generateUp();
-        console.log(wordsInBox);
       }
       else {
         generateDown();
-        console.log(wordsInBox);
       } // end of if else
 
       function generateUp() {
@@ -116,7 +114,6 @@ $(document).ready(function () {
     }
     else {
       goAheadAndGenerateText(wordCount);
-      console.log(wordsInBox);
     }
 
     function goAheadAndGenerateText(wordCount) {
